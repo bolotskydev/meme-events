81231696085d58227f95eeeb01dd3b634b0bb0fa
Kirill Bolotsky <kirill.bolotsky@gmail.com>
2019-09-17 20:45:10 +0500

    lib is working fine + done examples functionality

--- examples/index.js
+++ examples/index.js
@@ -1,17 +1,38 @@
-const eventCenter = {
-  toBeContinuedMemeEvent: window.toBeContinuedMemeEvent,
-  credits: window.toBeContinuedMemeEvent
-} 
+// bunch of helpers
 
+const currentEventState = (() => {
+  const state = {
+    el: document.getElementById('events-btn'),
+    event: window[document.getElementById('events-menu').value](),
+  }
+  return {
+    getElement: () => state.el,
+    setElement: newElement => (state.el = newElement),
+    getEvent: () => state.event,
+    setEvent: newEvent => (state.event = newEvent),
+  }
+})()
 
-document.onreadystatechange = () => {
-  $('#events-menu').on('change', e => {
-    // clear previous   
-  $('#events-btn')[0].on('click', e => {
-    toBeContinuedMemeEvent()({
-      fnOnStart: () => console.log('yeaaah'),
-      fnOnFinish: () => console.log('noooo'),
-    })
-  })
-  })
+const setCurrentEventState = (prevState, event) => {
+  prevState.setEvent(window[event]())
+}
+
+const boundEventToElement = (currentState = currentEventState) => {
+  currentState.getElement().addEventListener('click', currentState.getEvent())
+}
+
+const unboundEventFromElement = (currentState = currentEventState) => {
+  currentState
+    .getElement()
+    .removeEventListener('click', currentState.getEvent())
+}
+
+// main script
+
+boundEventToElement(currentEventState)
+
+document.getElementById('events-menu').onchange = e => {
+  unboundEventFromElement()
+  setCurrentEventState(currentEventState, e.target.value)
+  boundEventToElement()
 }
